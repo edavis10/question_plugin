de9f686a34abed89ffc5690c13484ea677d7d8f1
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-03 13:45:38 -0800

    Added migration to create the questions table. #1725

---
+++ db/migrate/001_create_questions.rb
@@ -0,0 +1,15 @@
+class CreateQuestions < ActiveRecord::Migration
+  def self.up
+    create_table :questions do |t|
+      t.column :journal_id, :integer
+      t.column :author, :integer
+      t.column :assigned_to_id, :integer
+      t.column :opened, :boolean, :default => true
+      t.column :issue_id, :integer
+    end
+  end
+
+  def self.down
+    drop_table :questions
+  end
+end
