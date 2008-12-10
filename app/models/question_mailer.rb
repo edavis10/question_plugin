e991ddeaf3deee67cffe4e5351bb3131001098e2
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-10 12:24:05 -0800

    Added QuestionMailer#asked_question to create a new question email using similar tempates to the issue update mails. #1711

--- app/models/question_mailer.rb
+++ app/models/question_mailer.rb
@@ -1,4 +1,12 @@
 class QuestionMailer < Mailer
-  def asked_question
+  def asked_question(question)
+    subject "[Question] #{question.issue.subject} ##{question.issue.id}"
+    recipients question.assigned_to.mail unless question.assigned_to.nil?
+    
+    body(:question => question,
+         :issue => question.issue,
+         :journal => question.journal,
+         :issue_url => url_for(:controller => 'issues', :action => 'show', :id => question.issue))
+    
   end
 end
--- spec/models/question_mailer_spec.rb
+++ spec/models/question_mailer_spec.rb
@@ -6,13 +6,60 @@ describe QuestionMailer do
   end
 end
 
-describe QuestionMailer, '#asked_question' do
-  it 'should create a mail message' do
-    mail = QuestionMailer.create_asked_question
-    mail.should be_an_instance_of(TMail::Mail)
+describe QuestionMailer, '#asked_question with a question' do
+  before(:each) do
+    @user = mock_model(User, :name => 'Test User', :mail => 'test@example.org')
+    @author = mock_model(User, :name => 'Author', :mail => 'author@example.org')
+    @tracker = mock_model(Tracker, :name => 'Bugs')
+    @issue = mock_model(Issue, :id => 1000, :tracker => @tracker, :author => nil, :subject => "Add new stuff", :status => nil, :priority => nil, :assigned_to => nil, :category => nil, :fixed_version => nil, :custom_values => [], :description => 'Issue description')
+    @journal = mock_model(Journal, :details => [], :notes => "This is the question for the user", :notes? => true)
+    @question = mock_model(Question, :assigned_to => @user, :author => @author, :issue => @issue, :journal => @journal)
+    
+    @mail = QuestionMailer.create_asked_question(@question)
+    Setting.stub!(:bcc_recipients?).and_return(true)
   end
   
-  it 'should have the prefix of [Question]'
-  it 'should be sent to the assigned_to user'
-  it 'should not be sent to the anyone user'
+  it 'should create a mail message' do
+    @mail.should be_an_instance_of(TMail::Mail)
+  end
+  
+  it 'should have the subject prefixed with [Question]' do
+    @mail.subject.should match(/\[Question\]/)
+  end
+  
+  it 'should use the issue subject for the subject line' do
+    @mail.subject.should match(/add new stuff/i)
+  end
+
+  it 'should use the issue id in the subject line' do
+    @mail.subject.should match(/#1000/)
+  end
+
+  it 'should be sent to the assigned_to user' do
+    @mail.bcc.should include(@user.mail)
+  end
+  
+  it 'should have a link to the issue' do
+    @mail.body.should match(/issues\/show\/1000/)
+  end
+
+  it 'should have a question in the body' do
+    @mail.body.should match(/is the question for the user/)
+  end
+end
+
+
+describe QuestionMailer, '#asked_question with a question for anyone' do
+  it 'should not be sent to anyone' do
+    author = mock_model(User, :name => 'Author', :mail => 'author@example.org')
+    tracker = mock_model(Tracker, :name => 'Bugs')
+    issue = mock_model(Issue, :id => 1000, :tracker => tracker, :author => nil, :subject => "Add new stuff", :status => nil, :priority => nil, :assigned_to => nil, :category => nil, :fixed_version => nil, :custom_values => [], :description => 'Issue description')
+    journal = mock_model(Journal, :details => [], :notes => "This is the question for the user", :notes? => true)
+    question = mock_model(Question, :assigned_to => nil, :issue => issue, :journal => journal, :author => author)
+    
+    @mail = QuestionMailer.create_asked_question(question)
+    @mail.to.should be_nil
+    @mail.cc.should be_nil
+    @mail.bcc.should be_nil
+  end
 end
