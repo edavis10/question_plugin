f5b5ec8dce629fffe12fe705b672b639b3eb3609
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-27 15:02:15 -0800

    Asked question emails will now only be sent out when the Journal is created.
      #2021

--- app/models/journal_questions_observer.rb
+++ app/models/journal_questions_observer.rb
@@ -3,7 +3,7 @@ require_dependency 'journal'
 class JournalQuestionsObserver < ActiveRecord::Observer
   observe :journal
   
-  def after_save(journal)
+  def after_create(journal)
     QuestionMailer.deliver_asked_question(journal) if journal.question
   end
 end
--- spec/models/journal_questions_observer_spec.rb
+++ spec/models/journal_questions_observer_spec.rb
@@ -6,19 +6,19 @@ describe JournalQuestionsObserver do
   end
 end
 
-describe JournalQuestionsObserver, '#after_save with no question' do
+describe JournalQuestionsObserver, '#after_create with no question' do
   it 'should do nothing' do
     journal = mock_model(Journal, :question => nil)
     QuestionMailer.should_not_receive(:deliver_asked_question)
-    JournalQuestionsObserver.instance.after_save(journal)
+    JournalQuestionsObserver.instance.after_create(journal)
   end
 end
 
-describe JournalQuestionsObserver, '#after_save with a question' do
+describe JournalQuestionsObserver, '#after_create with a question' do
   it 'should deliver an email' do
     question = mock_model(Question)
     journal = mock_model(Journal, :question => question)
     QuestionMailer.should_receive(:deliver_asked_question).with(journal)
-    JournalQuestionsObserver.instance.after_save(journal)
+    JournalQuestionsObserver.instance.after_create(journal)
   end
 end
