23a718c6b385619778b9b212c2556158ffc74eb5
Eric Davis <edavis@littlestreamsoftware.com>
2009-02-17 14:23:36 -0800

    Bumping version to 0.2.0

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :question_plugin do
   name 'Redmine Question plugin'
   author 'Eric Davis'
   description 'This is a plugin for Redmine that will allow users to ask questions to each other in issue notes'
-  version '0.1.0'
+  version '0.2.0'
 end
 
 ActiveRecord::Base.observers << :journal_questions_observer
