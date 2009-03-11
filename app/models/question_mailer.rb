4e28bc2649178756e25f46d9b405a851df4af336
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-10 18:15:47 -0700

    Fix for ActionMailer in 2.1.x re-rendering template implictly and not getting the body variables.

--- app/models/question_mailer.rb
+++ app/models/question_mailer.rb
@@ -5,12 +5,12 @@ class QuestionMailer < Mailer
     recipients question.assigned_to.mail unless question.assigned_to.nil?
     @from  = "#{question.author.name} (Redmine) <#{Setting.mail_from}>" unless question.author.nil?
 
-    body = {
+    body({
       :question => question,
       :issue => question.issue,
       :journal => journal,
       :issue_url => url_for(:controller => 'issues', :action => 'show', :id => question.issue)
-    }
+    })
 
     part :content_type => "text/plain", :body => render_message("asked_question.erb", body)
     part :content_type => "text/html", :body => render_message("asked_question.text.html.rhtml", body)
@@ -23,12 +23,12 @@ class QuestionMailer < Mailer
     recipients question.author.mail unless question.author.nil?
     @from = "#{question.assigned_to.name} (Redmine) <#{Setting.mail_from}>" unless question.assigned_to.nil?
 
-    body = {
+    body({
       :question => question,
       :issue => question.issue,
       :journal => closing_journal,
       :issue_url => url_for(:controller => 'issues', :action => 'show', :id => question.issue)
-    }
+    })
 
     part :content_type => "text/plain", :body => render_message("answered_question.erb", body)
     part :content_type => "text/html", :body => render_message("answered_question.text.html.rhtml", body)
