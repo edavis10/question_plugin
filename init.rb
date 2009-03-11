ee01ca0e495899446a8e919825267ce116a8afa5
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-10 19:31:19 -0700

    Bumped version to 0.3.0 and added some plugin metadata. #2236

--- init.rb
+++ init.rb
@@ -11,8 +11,13 @@ require 'question_journal_hooks'
 Redmine::Plugin.register :question_plugin do
   name 'Redmine Question plugin'
   author 'Eric Davis'
+  url "https://projects.littlestreamsoftware.com/projects/redmine-questions" if respond_to?(:url)
+  author_url 'http://www.littlestreamsoftware.com' if respond_to?(:author_url)
   description 'This is a plugin for Redmine that will allow users to ask questions to each other in issue notes'
-  version '0.2.0'
+  version '0.3.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
+
 end
 
 ActiveRecord::Base.observers << :journal_questions_observer
