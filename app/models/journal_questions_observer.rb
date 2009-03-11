247b676cdeb9ba550bb772d91207b2361175a84b
Eric Davis <edavis@littlestreamsoftware.com>
2009-03-10 18:15:05 -0700

    Fix for 0.8 where the question wasn't getting saved with the journal.

--- app/models/journal_questions_observer.rb
+++ app/models/journal_questions_observer.rb
@@ -4,7 +4,10 @@ class JournalQuestionsObserver < ActiveRecord::Observer
   observe :journal
   
   def after_create(journal)
-    QuestionMailer.deliver_asked_question(journal) if journal.question
+    if journal.question
+      journal.question.save
+      QuestionMailer.deliver_asked_question(journal)
+    end
 
     # Close any open questions
     if journal.issue && journal.issue.pending_question?(journal.user)
