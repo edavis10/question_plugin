d04bfbc4466012200dc0a9dff1a1d850fdab6ba3
Eric Davis <edavis@littlestreamsoftware.com>
2010-04-05 10:10:45 -0700

    [#3601] Replace text ? with an actual icon.

--- lib/question_kanban_hooks.rb
+++ lib/question_kanban_hooks.rb
@@ -1,33 +1,30 @@
 class QuestionKanbanHooks < QuestionHooksBase
   def view_kanbans_issue_details(context = {})
-# GREY when there are no questions - (question count is 0)
-# RED when all questions unanswered - (question count is equal to the unanswered question count)
-# GREEN when all questions is answered and no new note by the "assigned to" person - (question count is not 0 and unanswered question count is 0 and the last journal author is not the assigned to)
-# ORANGE if some questions are answered and the "assigned to" person has added a note - (question count is not 0 and unanswered question count is less than the total question count and the last journal author is the assigned to)
-# BLACK if all questions are answered and the "assigned to" person has added a note - (question count is not 0 and unanswered question count is 0 and the last journal author is the assigned to)
-# A Tool-tip that displays how many questions are answered/unanswered (e.g. 2/4 questions answered, 0 questions, 4/4 questions answered)
-
-    
+    # GREY when there are no questions - (question count is 0)
+    # RED when all questions unanswered - (question count is equal to the unanswered question count)
+    # GREEN when all questions is answered and no new note by the "assigned to" person - (question count is not 0 and unanswered question count is 0 and the last journal author is not the assigned to)
+    # ORANGE if some questions are answered and the "assigned to" person has added a note - (question count is not 0 and unanswered question count is less than the total question count and the last journal author is the assigned to)
+    # BLACK if all questions are answered and the "assigned to" person has added a note - (question count is not 0 and unanswered question count is 0 and the last journal author is the assigned to)
     issue = context[:issue]
 
     if issue.questions.count == 0
-      return question_icon('c6c6c6', issue) # gray
+      return question_icon(:gray, issue)
     end
     
     if issue.questions.count == issue.open_questions.count
-      return question_icon('ff0000', issue) # red
+      return question_icon(:red, issue)
     end
 
     if issue.questions.count != 0 && issue.open_questions.count == 0 && issue.journals.last && issue.journals.last.user != issue.assigned_to
-      return question_icon('005829', issue) # green
+      return question_icon(:green, issue)
     end
 
     if issue.questions.count != 0 && issue.open_questions.count <= issue.questions.count && issue.journals.last.user == issue.assigned_to
-      return question_icon('dd6a06', issue) # orange
+      return question_icon(:orange, issue)
     end
 
     if issue.open_questions.count == 0 && issue.journals.last.user == issue.assigned_to
-      return question_icon('000000', issue) # black
+      return question_icon(:black, issue)
     end
 
     return ''
@@ -63,6 +60,7 @@ class QuestionKanbanHooks < QuestionHooksBase
     answered_questions = total_questions - open_questions
     
     title = l(:question_text_ratio_questions_answered, :ratio => "#{answered_questions}/#{total_questions}")
-    "<span class='kanban-question' style='color: ##{color}' title='#{title}'>?</span>"
+    link_to(image_tag("question-#{color}.png", :plugin => 'question_plugin', :title => title, :class => 'kanban-question'),
+            { :controller => 'issues', :action => 'show', :id => issue })
   end
 end
