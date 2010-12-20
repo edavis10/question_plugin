c8cb2ead79844c535d03d885429222e744fff24e
Eric Davis <edavis@littlestreamsoftware.com>
2010-12-20 15:51:08 -0800

    [#4263] Use the issue-show-popup form on the Kanban for questions

--- lib/question_kanban_hooks.rb
+++ lib/question_kanban_hooks.rb
@@ -53,6 +53,7 @@ class QuestionKanbanHooks < QuestionHooksBase
     
     title = l(:question_text_ratio_questions_answered, :ratio => "#{answered_questions}/#{total_questions}")
     link_to(image_tag("question-#{color}.png", :plugin => 'question_plugin', :title => title, :class => "kanban-question #{color}"),
-            { :controller => 'issues', :action => 'show', :id => issue })
+            { :controller => 'issues', :action => 'show', :id => issue },
+            :class => "issue-show-popup issue-#{h(issue.id)}")
   end
 end
