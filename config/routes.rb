2d517af7b09a0ea62b514d348b8f67a99c8a9ad6
cforce <cforce@gmx.de>
2012-07-25 17:13:49 +0200

    added redmine 1.4 and 2.0 comatible routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-match 'questions/autocomplete_for_user_login/project/:id/issue/:issue_id' => 'questions#autocomplete_for_user_login', :format => false, :as => 'questions_autocomplete_for_user_login'
-match 'questions/my_issue_filter(/project/:project)' => 'questions#my_issue_filter', :format => false, :as => 'questions_my_issue_filter'
-match 'questions/user_issue_filter/user/:user_id' => 'questions#user_issue_filter', :format => false, :as => 'questions_user_issue_filter'
+if Rails::VERSION::MAJOR < 3
+  ActionController::Routing::Routes.draw do |map|
+	map.connect 'questions/autocomplete_for_user_login/project/:id/issue/:issue_id',:controller=> 'questions', :action=> 'autocomplete_for_user_login'
+	map.connect 'questions/my_issue_filter(/project/:project)' ,:controller=> 'questions', :action=> 'my_issue_filter'
+	map.connect 'questions/user_issue_filter/user/:user_id' ,:controller=> 'questions', :action=> 'user_issue_filter'
+  end
+else
+	match 'questions/autocomplete_for_user_login/project/:id/issue/:issue_id' => 'questions#autocomplete_for_user_login', :format => false, :as => 'questions_autocomplete_for_user_login'
+	match 'questions/my_issue_filter(/project/:project)' => 'questions#my_issue_filter', :format => false, :as => 'questions_my_issue_filter'
+	match 'questions/user_issue_filter/user/:user_id' => 'questions#user_issue_filter', :format => false, :as => 'questions_user_issue_filter'
+end
+
