67bd45e8dd0c6af189c225957ba0732e8cca7527
Stephen Leavitt <stephen.leavitt@iostudio.com>
2013-01-12 19:51:32 -0600

    Minor spacing change for readability

--- lib/question_hooks_base.rb
+++ lib/question_hooks_base.rb
@@ -8,14 +8,14 @@ class QuestionHooksBase < Redmine::Hook::ViewListener
     html = "<span class=\"question-line\">"
     html << " #{l(:text_question_for)} "
     html << link_to_user(question.assigned_to)
-    html << " <span>#{avatar(question.assigned_to, { :size => 24, :class => '' })}</span> </span>" if question.assigned_to && question.assigned_to.mail
+    html << " <span>#{avatar(question.assigned_to, { :size => 24, :class => '' })}</span>&nbsp;</span>" if question.assigned_to && question.assigned_to.mail
     html
   end
   
   def unassigned_question_html(question)
     html = "<span class=\"question-line\">"
     html << l(:text_question_for_anyone)
-    html << "</span>"
+    html << "&nbsp;</span>"
     html
   end
 end
