80caa6cdbfc06e78a53a5f8562c38e18342403b6
Dmitry Salashnik <salashnik@nexstep.ua>
2014-02-20 10:42:59 +0200

    Fix internal error when "questions" column visible in issue list (in Redmine > 2.3)

--- lib/question_plugin/patches/queries_helper_patch.rb
+++ lib/question_plugin/patches/queries_helper_patch.rb
@@ -7,6 +7,7 @@ module QuestionPlugin
         base.send(:include, InstanceMethods)
     
         base.class_eval do
+          unloadable 
           alias_method_chain :column_content, :question
         end
       end
@@ -15,40 +16,42 @@ module QuestionPlugin
       end
       
       module InstanceMethods
+        
+
+        # removed format_questions for compatibility with redmine >= 2.3
+        # see http://www.redmine.org/boards/3/topics/37345, 
+        #     http://www.redmine.org/issues/13753       
         def column_content_with_question(column, issue)
           if column.name == :formatted_questions
-            return format_questions(issue.open_questions)
+            return '' if issue.open_questions.empty?
+            html = '<ol>'
+            issue.open_questions.each do |question|
+              html << "<li>"
+              html << "  <div class='tooltip'>"
+              html << "    <span class='question_summary'>"
+              html << link_to(h(truncate(question.journal.notes, :length => Question::TruncateTo)),
+                              :controller => 'issues',
+                              :action => 'show',
+                              :id => question.issue,
+                              :anchor => "question-#{question.id}")
+              html << "    </span>"
+              html << "    <span class='tip'>"
+              html << link_to_issue(question.issue)
+              html << ": #{h(question.journal.notes)}<br /><br />"
+              html << "<strong>#{l(:question_text_asked_by)}</strong>: #{question.author.to_s}<br />"
+              html << "<strong>#{l(:question_text_assigned_to)}</strong>: #{question.assigned_to.to_s}<br />"
+              html << "<strong>#{l(:question_text_created_on)}</strong>: #{format_date(question.journal.created_on)}"
+              html << "    </span>"
+              html << "  </div>"
+              html << "</li>"
+            end
+            html << '</ol>'
+            return html
           else
             column_content_without_question(column, issue)
           end
         end
         
-        def format_questions(questions)
-          return '' if questions.empty?
-          html = '<ol>'
-          questions.each do |question|
-            html << "<li>"
-            html << "  <div class='tooltip'>"
-            html << "    <span class='question_summary'>"
-            html << link_to(h(truncate(question.journal.notes, :length => Question::TruncateTo)),
-                            :controller => 'issues',
-                            :action => 'show',
-                            :id => question.issue,
-                            :anchor => "question-#{question.id}")
-            html << "    </span>"
-            html << "    <span class='tip'>"
-            html << link_to_issue(question.issue)
-            html << ": #{h(question.journal.notes)}<br /><br />"
-            html << "<strong>#{l(:question_text_asked_by)}</strong>: #{question.author.to_s}<br />"
-            html << "<strong>#{l(:question_text_assigned_to)}</strong>: #{question.assigned_to.to_s}<br />"
-            html << "<strong>#{l(:question_text_created_on)}</strong>: #{format_date(question.journal.created_on)}"
-            html << "    </span>"
-            html << "  </div>"
-            html << "</li>"
-          end
-          html << '</ol>'
-          return html
-        end
       end
     end
   end
