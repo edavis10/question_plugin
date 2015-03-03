c08703435eddfd9dc9d03bac5b7b096e3e03da4c
ichizok <gclient.gaap@gmail.com>
2015-03-04 05:56:51 +0900

    append 'via' parameter to match method

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
-if Rails::VERSION::MAJOR < 3
-  ActionController::Routing::Routes.draw do |map|
-    map.connect 'issuequestions/autocomplete_for_user_login/project/:id/issue/:issue_id', :controller=> 'issuequestions', :action=> 'autocomplete_for_user_login'
-    map.connect 'issuequestions/my_issue_filter(/project/:project)' ,:controller=> 'issuequestions', :action=> 'my_issue_filter'
-    map.connect 'issuequestions/user_issue_filter/user/:user_id' ,:controller=> 'issuequestions', :action=> 'user_issue_filter'
-    map.connect 'issuequestions/hide/:id', :controller => 'issuequestions', :action => 'hide'
-  end
-else
-	match 'issuequestions/autocomplete_for_user_login/project/:id/issue/:issue_id' => 'issuequestions#autocomplete_for_user_login', :format => false, :as => 'issuequestions_autocomplete_for_user_login'
-	match 'issuequestions/my_issue_filter(/project/:project)' => 'issuequestions#my_issue_filter', :format => false, :as => 'questions_my_issue_filter'
-	match 'issuequestions/user_issue_filter/user/:user_id' => 'issuequestions#user_issue_filter', :format => false, :as => 'questions_user_issue_filter'
-  match 'issuequestions/hide/:id' => 'issuequestions#hide', :as => 'hide'
+RedmineApp::Application.routes.draw do
+  match 'issuequestions/autocomplete_for_user_login/project/:id/issue/:issue_id' => 'issuequestions#autocomplete_for_user_login', :format => false, :as => 'issuequestions_autocomplete_for_user_login', :via => [:get, :post]
+  match 'issuequestions/my_issue_filter(/project/:project)' => 'issuequestions#my_issue_filter', :format => false, :as => 'questions_my_issue_filter', :via => [:get, :post]
+  match 'issuequestions/user_issue_filter/user/:user_id' => 'issuequestions#user_issue_filter', :format => false, :as => 'questions_user_issue_filter', :via => [:get, :post]
+  match 'issuequestions/hide/:id' => 'issuequestions#hide', :as => 'hide', :via => [:get, :post]
 end
-
